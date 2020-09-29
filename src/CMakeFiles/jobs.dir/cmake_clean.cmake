fca200a5b29030f5a4e91361faecf28f5cea5ed3
preyansh98 <preyansh98@gmail.com>
2020-09-29 16:13:49 -0400

    makefile done

---
+++ src/CMakeFiles/jobs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jobs.dir/jobs/job.c.o"
+  "libjobs.pdb"
+  "libjobs.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jobs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
